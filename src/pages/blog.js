bea04f01a53ca376762a6ea3132b762d879efadc
Sara <sahara.tijol@gmail.com>
2020-12-13 23:55:20 -0600

    render array of objects with react

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import Layout from '../components/layout'
+import {graphql, useStaticQuery} from "gatsby";
 
 const BlogPage = () => {
+
+    const data = useStaticQuery(graphql `
+        query {
+            allMarkdownRemark {
+                edges {
+                    node{
+                        frontmatter {
+                            title,
+                            date
+                            }
+                            html
+                            excerpt
+                            }}}}
+    `)
+
     return (
         <Layout>
             <h1>Blog</h1>
             <p>Posts will show up here.. a lists</p>
+            <ol>
+                {data.allMarkdownRemark.edges.node.frontmatter.title}
+                {data.allMarkdownRemark.edges.node.frontmatter.date}
+            </ol>
         </Layout>
     )
 }
