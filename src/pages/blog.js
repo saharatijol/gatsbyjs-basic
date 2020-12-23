bfaf27fa65d02959c8b2fe8261d620043ac9cf41
Sara <sahara.tijol@gmail.com>
2020-12-23 00:30:23 -0600

    Complete mini challenge: Link posts by fetching slug for posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
-import {graphql, useStaticQuery} from "gatsby";
+import {Link, graphql, useStaticQuery} from "gatsby";
 
 const BlogPage = () => {
 
@@ -12,9 +12,14 @@ const BlogPage = () => {
                         frontmatter {
                             title,
                             date
-                            }
-                            
-                            }}}}
+                        }
+                        fields {
+                            slug
+                        }         
+                    }
+                }
+            }
+        }
     `)
 
     return (
@@ -24,8 +29,10 @@ const BlogPage = () => {
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return (
                         <li>
+                            <Link to={`/blog/${edge.node.fields.slug}`}>
                             <h2>{edge.node.frontmatter.title}</h2>
                             <p>{edge.node.frontmatter.date}</p>
+                            </Link>
                         </li>
                     )
                 })}
