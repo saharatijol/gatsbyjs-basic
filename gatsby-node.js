a2d093d4f9bb9eaeae6bd43650512814841e7642
Sara <sahara.tijol@gmail.com>
2020-12-22 23:55:29 -0600

    Only tap into node internal APIs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,12 @@
 
-// Generate slug field
+// Generate slug field - tap into node internal APIs
+// Check out API Reference from Gatsby docs - Gatsby Node APIs
+
+// this function runs when a new node is created
 module.exports.onCreateNode = ({ node, actions }) => {
     const { createNodeField } = actions
 
-    console.log(JSON.stringify(node, undefined, 4))
+    if (node.internal.type === 'MarkdownRemark') {
+        console.log(JSON.stringify(node, undefined, 4))
+    }
 }
