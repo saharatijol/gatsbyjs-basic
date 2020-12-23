6453f377dcd5161bf48be37f13790fea2ca00df6
Sara <sahara.tijol@gmail.com>
2020-12-23 00:23:45 -0600

    Generating slugs to create pages by exporting node internal apis

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,53 @@
+const path = require('path')
 
 // Generate slug field - tap into node internal APIs
 // Check out API Reference from Gatsby docs - Gatsby Node APIs
 
 // this function runs when a new node is created
-module.exports.onCreateNode = ({ node, actions }) => {
-    const { createNodeField } = actions
+module.exports.onCreateNode = ({node, actions}) => {
+    const {createNodeField} = actions
 
     if (node.internal.type === 'MarkdownRemark') {
-        console.log(JSON.stringify(node, undefined, 4))
+        const slug = path.basename(node.fileAbsolutePath, '.md')
+
+        // console.log('@@@@@@@@@@@@', slug)
+        createNodeField({
+            node,
+            name: 'slug',
+            value: slug
+        })
     }
+}
+
+// this is a different graphql, this is a graphql function
+module.exports.createPages = async ({graphql, actions}) => {
+    const {createPage} = actions
+    const blogTemplate = path.resolve('./src/templates/blog.js')
+    const res = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug 
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    res.data.allMarkdownRemark.edges.forEach((edge) => {
+        createPage({
+            component: blogTemplate,
+            path: `/blog/${edge.node.fields.slug}`,
+            context: {
+                slug: edge.node.fields.slug
+            }
+        })
+    })
+
+    // 1. Get path to template
+    // 2. Get markdown data
+    // 3. Create new pages
 }
